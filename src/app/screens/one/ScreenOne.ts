a3bed8d7d13854761c1506d9a0a5fdb5e4e0b76c
thalys <thalys@ppd.gg>
2025-09-11 14:46:54 -0300

    clean up `ScreenOne`

--- src/app/screens/one/ScreenOne.ts
+++ src/app/screens/one/ScreenOne.ts
@@ -1,134 +1,37 @@
 import type { Ticker } from 'pixi.js'
-import { FancyButton } from '@pixi/ui'
-import { animate } from 'motion'
-import { Container } from 'pixi.js'
-import { engine } from '@/app/getEngine'
-import { PausePopup } from '@/app/popups/PausePopup'
-import { SettingsPopup } from '@/app/popups/SettingsPopup'
-import { Bouncer } from '@/app/screens/main/Bouncer'
-import { Button } from '@/app/ui/Button'
+import { ScreenBaseUI } from '@/app/screens/ScreenBaseUI'
 
-/** The screen that holds the app */
-export class ScreenOne extends Container {
+/**
+ * Screen with the first assignment
+ */
+export class ScreenOne extends ScreenBaseUI {
   /** Assets bundles required by this screen */
-  public static assetBundles = ['main']
-  public mainContainer: Container
-  private pauseButton: FancyButton
-  private settingsButton: FancyButton
-  private addButton: FancyButton
-  private removeButton: FancyButton
-  private bouncer: Bouncer
-  private paused = false
+  public static override assetBundles = ['main']
 
   constructor () {
     super()
-
-    this.mainContainer = new Container()
-    this.addChild(this.mainContainer)
-    this.bouncer = new Bouncer()
-
-    const buttonAnimations = {
-      hover: {
-        props: { scale: { x: 1.1, y: 1.1 } },
-        duration: 100,
-      },
-      pressed: {
-        props: { scale: { x: 0.9, y: 0.9 } },
-        duration: 100,
-      },
-    }
-    this.pauseButton = new FancyButton({
-      defaultView: 'icon-pause.png',
-      anchor: 0.5,
-      animations: buttonAnimations,
-    })
-    this.pauseButton.onPress.connect(() => {
-      void engine().navigation.presentPopup(PausePopup)
-    })
-    this.addChild(this.pauseButton)
-
-    this.settingsButton = new FancyButton({
-      defaultView: 'icon-settings.png',
-      anchor: 0.5,
-      animations: buttonAnimations,
-    })
-    this.settingsButton.onPress.connect(() => {
-      void engine().navigation.presentPopup(SettingsPopup)
-    })
-    this.addChild(this.settingsButton)
-
-    this.addButton = new Button({
-      text: 'Add',
-      width: 175,
-      height: 110,
-    })
-    this.addButton.onPress.connect(() => this.bouncer.add())
-    this.addChild(this.addButton)
-
-    this.removeButton = new Button({
-      text: 'Remove',
-      width: 175,
-      height: 110,
-    })
-    this.removeButton.onPress.connect(() => this.bouncer.remove())
-    this.addChild(this.removeButton)
   }
 
   /** Prepare the screen just before showing */
-  public prepare () {}
+  public override prepare () {}
 
   /** Update the screen */
-  public update (_time: Ticker) {
-    if (this.paused) { return }
-    this.bouncer.update()
-  }
+  public override update (time: Ticker) {
 
-  /** Pause gameplay - automatically fired when a popup is presented */
-  public async pause () {
-    this.mainContainer.interactiveChildren = false
-    this.paused = true
-  }
-
-  /** Resume gameplay */
-  public async resume () {
-    this.mainContainer.interactiveChildren = true
-    this.paused = false
   }
 
   /** Fully reset */
-  public reset () {}
+  public override reset () {}
 
   /** Resize the screen, fired whenever window size changes */
-  public resize (width: number, height: number) {
-    const mcx = width * 0.5
-    const mcy = height * 0.5
-
-    this.mainContainer.x = mcx
-    this.mainContainer.y = mcy
-    this.pauseButton.x = 30
-    this.pauseButton.y = 30
-    this.settingsButton.x = width - 30
-    this.settingsButton.y = 30
-
-    this.bouncer.resize(width, height)
+  public override resize (width: number, height: number) {
+    super.resize(width, height)
   }
 
   /** Show screen with animations */
-  public async show (): Promise<void> {
-    const elementsToAnimate = [
-      this.pauseButton,
-      this.settingsButton,
-    ]
+  public override async show (): Promise<void> {
 
-    for (const element of elementsToAnimate) {
-      element.alpha = 0
-      animate(
-        element,
-        { alpha: 1 },
-        { duration: 0.3, delay: 0.75, ease: 'backOut' },
-      )
-    }
   }
 
-  public async hide () {}
+  public override async hide () {}
 }
