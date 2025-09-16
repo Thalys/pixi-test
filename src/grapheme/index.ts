be141109f9067370c3a89a8a5dc4eb76bd52cd9b
thalys <thalys@ppd.gg>
2025-09-16 16:24:23 -0300

    clean up

--- src/grapheme/index.ts
+++ src/grapheme/index.ts
@@ -1,13 +1,9 @@
-import { Text, TextStyle } from 'pixi.js'
-import { engine } from '@/engine/engine.singleton'
 import { createApp } from '@/grapheme/index.app'
-import { createBasic, createBasicMonospace, createRichText, createTextSkew } from '@/grapheme/index.elements'
+import { createBasic, createBasicMonospace } from '@/grapheme/index.elements'
 
 (async () => {
 
   await createApp()
-
-  const { stage, screen } = engine()
 
   const textBasic = createBasic()
 
