d1a24993cd6db958d3c91a39a9b225e64edf3cc6
thalys <thalys@ppd.gg>
2025-09-19 10:44:37 -0300

    add main.create.ts and update main.ts to use new creation flow

---
+++ src/main.create.ts
@@ -0,0 +1,29 @@
+import { initDevtools } from '@pixi/devtools'
+import { getAppVersion, isDev } from '@/app/global'
+import { ZINC } from '@/app/utils/colors'
+import { CreationEngine } from '@/engine/engine'
+import { setEngine } from '@/engine/engine.singleton'
+import { Measure } from '@/engine/utils/stage-ruler'
+import { logger } from '@/tools/logger'
+import '@/app/extra-modules'
+
+export async function createApplication () {
+  const engine = new CreationEngine()
+  setEngine(engine)
+
+  await engine.init({
+    resizeOptions: { minWidth: 768, minHeight: 1024, letterbox: false },
+    background: ZINC[900],
+    antialias: true,
+    hello: isDev(),
+    powerPreference: 'high-performance',
+  })
+
+  initDevtools({ app: engine })
+
+  if (isDev()) {
+    logger.table(JSON.parse(JSON.stringify(import.meta.env)))
+    logger.info(JSON.parse(JSON.stringify(`App version: \n${getAppVersion()}`)))
+    engine.navigation.setMeasureLayer(Measure)
+  }
+}
--- src/main.ts
+++ src/main.ts
@@ -1,33 +1,15 @@
-import { initDevtools } from '@pixi/devtools'
-import { getAppVersion } from '@/app/global'
 import { LoadScreen } from '@/app/screens/loading/ScreenLoad'
 import { userSettings } from '@/app/utils/user.settings'
-import { CreationEngine } from '@/engine/engine'
-import { setEngine } from '@/engine/engine.singleton'
-import { Measure } from '@/engine/utils/stage-ruler'
-import { logger } from '@/tools/logger'
-import '@/app/extra-modules'
+import { engine } from '@/engine/engine.singleton'
+import { createApplication } from '@/main.create'
 
 (async () => {
-  const engine = new CreationEngine()
-  setEngine(engine)
-
-  await engine.init({
-    background: '#1E1E1E',
-    resizeOptions: { minWidth: 768, minHeight: 1024, letterbox: false },
-  })
-
-  if (import.meta.env.DEV) {
-    logger.table(JSON.parse(JSON.stringify(import.meta.env)))
-    logger.info(JSON.parse(JSON.stringify(`App version: \n${getAppVersion()}`)))
-    engine.navigation.setMeasureLayer(Measure)
-  }
-
-  initDevtools({ app: engine })
+  await createApplication()
 
   // Initialize the user settings
   userSettings.init()
 
-  await engine.navigation.showScreen(LoadScreen)
-  await engine.navigation.showLastSessionScreen()
+  const { navigation } = engine()
+  await navigation.showScreen(LoadScreen)
+  await navigation.showLastSessionScreen()
 })()
