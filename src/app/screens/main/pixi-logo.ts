b97e2748d7370b6ce5e0f1295bb0e926dfbcba21
thalys <thalys@ppd.gg>
2025-09-26 11:27:36 -0300

    refactor pixi-logo to use `anime`

--- src/app/screens/main/pixi-logo.ts
+++ src/app/screens/main/pixi-logo.ts
@@ -1,43 +1,28 @@
 import type { FederatedPointerEvent } from 'pixi.js'
-import { animate } from 'motion'
 import { Container, Sprite, Texture } from 'pixi.js'
-
-const fadeIn = async (element: Container) => {
-  await animate(
-    element,
-    { alpha: 1 },
-    { duration: 0.5, ease: 'circOut' },
-  )
-}
-const fadeOut = async (element: Container) => {
-  animate(
-    element,
-    { alpha: 0 },
-    { duration: 0.5, ease: 'backOut' },
-  )
-}
+import { anime } from '@/anime/anime'
 
 const goToPixiWebsite = () => {
   window.open('https://pixijs.com', '_blank')
 }
 
-export class Logo extends Container {
+export class PixiLogo extends Container {
 
   private bg: Sprite
   private fg: Sprite
 
   onPointerOver = (event: FederatedPointerEvent) => {
-    fadeIn(this.fg)
+    anime`fade-in duration-0.3`(this.fg).play()
   }
 
   onPointerOut = (event: FederatedPointerEvent) => {
-    fadeOut(this.fg)
+    anime`fade-out`(this.fg).play()
   }
 
   constructor () {
     super()
 
-    this.interactive = true
+    // this.interactive = true
     this.cursor = 'pointer'
 
     this.bg = Sprite.from(Texture.from('logo.svg'))
